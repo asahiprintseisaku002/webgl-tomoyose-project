8e0ced7f31a5b5bae81990eab50b4563b3203a0b
github <166389720+asahiprintseisaku002@users.noreply.github.com.>
2024-06-02 17:45:16 +0900

    モバイル修正

--- 02/script-240602.js
+++ 02/script-240602.js
@@ -364,23 +364,22 @@ class ThreeApp {
             document.querySelector('.text').style.opacity = '1';
         }
     });
-/*
+
     // タップ操作を検出できるようにする
-    window.addEventListener('touchstart', (touchEvent) => {
-      if (self.isAnimating) { 
-          self.isDown = true;
-          self.isAnimating = !self.isAnimating;
-          document.querySelector('.text').style.opacity = '0';
-      }
-  }, false);
-  
-  window.addEventListener('touchend', (touchEvent) => {
-      if (!self.isAnimating ) {
-          self.isDown = false;
-          document.querySelector('.text').style.opacity = '1';
-      }
-  }, false);
-*/
+    window.addEventListener('touchstart', function(event) {
+        self.isDown = true;
+        self.isAnimating = !self.isAnimating;
+        document.querySelector('.text').style.opacity = '0';
+    });
+
+    // タッチ終了を検出
+    window.addEventListener('touchend', function(event) {
+        if (!self.isAnimating) {
+            self.isDown = false;
+            document.querySelector('.text').style.opacity = '1';
+        }
+    });
+
     // ウィンドウのリサイズを検出できるようにする
     window.addEventListener('resize', () => {
       this.renderer.setSize(window.innerWidth, window.innerHeight);
